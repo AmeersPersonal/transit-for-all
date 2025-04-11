f3e39fdd5660f95e2751f456db8ddced71e7d562
Jc-10243296 <jeffrey.chu06@gmail.com>
2025-04-11 13:27:24 -0400

    Pushing FE

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -3,10 +3,10 @@ import Clickable from '@/components/Clickable';
 const FlatListBasics = () => {
   return (
     <>
-      <Clickable title="Press Me" onPress={() => alert('Button pressed!')}/>
-      <Clickable title="Press Me" onPress={() => alert('Button pressed!')}/>
-      <Clickable title="Press Me" onPress={() => alert('Button pressed!')}/>
-      <Clickable title="Press Me" onPress={() => alert('Button pressed!')}/>
+      <Clickable title="StationA" accessibilityDetails={['Elevators: Yes', 'Escalators: Yes', 'Info: Yes']} onPress={() => alert('Button pressed!')}/>
+      <Clickable title="StationB" accessibilityDetails={['Elevators: Yes', 'Escalators: Yes', 'Info: Yes']} onPress={() => alert('Button pressed!')}/>
+      <Clickable title="StationC" accessibilityDetails={['Elevators: Yes', 'Escalators: Yes', 'Info: Yes']} onPress={() => alert('Button pressed!')}/>
+      <Clickable title="StationD" accessibilityDetails={['Elevators: Yes', 'Escalators: Yes', 'Info: Yes']} onPress={() => alert('Button pressed!')}/>
     </>
 
   );
